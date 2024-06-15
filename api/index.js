dc3612476aa4e75dde2b75d6aa96ebd1a47f4306
Satender singh <75835926+Satender1998@users.noreply.github.com>
2024-06-15 16:15:28 +0530

    init

--- api/index.js
+++ api/index.js
@@ -78,7 +78,7 @@ app.get("/newstories", async (req, res) => {
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 })
 
 
