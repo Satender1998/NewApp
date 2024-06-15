e580492f79a532ae720b4fd8fc6d19d699d9f095
Satender singh <75835926+Satender1998@users.noreply.github.com>
2024-06-15 16:41:12 +0530

    init

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3001',
+//         secure: false,
+//       },
+//     },
+//   },
+
+//   plugins: [
+//     react(),
+    
+//   ],
+// })
+
+
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
